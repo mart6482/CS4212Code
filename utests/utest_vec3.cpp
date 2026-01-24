22d821df90963d5bb9e37150edc3d6595c1d1bf2
Andrew Martini <mart6482@d.umn.edu>
2026-01-23 20:11:04 -0600

    Added more unit tests

--- utests/utest_vec3.cpp
+++ utests/utest_vec3.cpp
@@ -22,3 +22,38 @@ TEST_CASE("Vec3 Multiplication Test"){
     REQUIRE(v3.z() == 0.0);
 }
 
+TEST_CASE("Vec3 Length/Squaring Test"){
+    vec3 v(3.0, 4.0, 0.0);
+
+    REQUIRE(v.length_squared() == 25.0);
+    REQUIRE_THAT(v.length(), Catch::Matchers::WithinAbs(5.0, 1.0e-3));
+    
+}
+
+TEST_CASE("Vec3 Negation Test"){
+    vec3 v1(1.0, -2.0, 3.0);
+    vec3 v2 = -v1;
+
+    REQUIRE(v2.x() == -1.0);
+    REQUIRE(v2.y() == 2.0);
+    REQUIRE(v2.z() == -3.0);
+}
+
+TEST_CASE("Vec3 Division Test"){
+    vec3 v1(2.0, 4.0, 6.0);
+    double t = 2.0;
+    v1 /= t;
+
+    REQUIRE(v1.x() == 1.0);
+    REQUIRE(v1.y() == 2.0);
+    REQUIRE(v1.z() == 3.0);
+}
+
+
+TEST_CASE("Vec3 Dot Product Test"){
+    vec3 v1(1.0, 2.0, 3.0);
+    vec3 v2(4.0, -5.0, 6.0);
+    double dot_product = dot(v1, v2);
+
+    REQUIRE(dot_product == 12.0);
+}

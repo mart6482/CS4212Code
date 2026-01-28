73777b42094f019472e65bb82e5ed38b584c8479
Andrew Martini <mart6482@d.umn.edu>
2026-01-28 17:25:56 -0600

    added the color utility function

---
+++ src/color.h
@@ -0,0 +1,20 @@
+#ifndef COLOR.H
+#define COLOR.H
+#include "vec3.h"
+#include <iostream>
+
+using color = vec3;
+
+void write_color(std::ostream &out, color pixel_color){
+    auto r = pixel_color.x();
+    auto g = pixel_color.y();
+    auto b = pixel_color.z();
+
+    int rbyte = int(255.999 * r);
+    int gbyte = int(255.999 * g);
+    int bbyte = int(255.999 * b);
+
+    out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
+}
+
+#endif

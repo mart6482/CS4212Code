31d48088d8b6da86de148c2030ee1c3002a580f4
Andrew Martini <mart6482@d.umn.edu>
2026-01-30 21:26:33 -0600

    Fixed the Ray Utests to check if the calculations are within floating-point tolerance

--- utests/utest_ray.cpp
+++ utests/utest_ray.cpp
@@ -8,13 +8,13 @@ TEST_CASE("Ray Construction Test"){
     vec3 dir(4.0, 5.0, 6.0);
     ray r(orig, dir);
 
-    REQUIRE(r.getOrigin().x() == 1.0);
-    REQUIRE(r.getOrigin().y() == 2.0);
-    REQUIRE(r.getOrigin().z() == 3.0);
+    REQUIRE_THAT(r.getOrigin().x(), Catch::Matchers::WithinAbs(1.0, 1e-8));
+    REQUIRE_THAT(r.getOrigin().y(), Catch::Matchers::WithinAbs(2.0, 1e-8));
+    REQUIRE_THAT(r.getOrigin().z(), Catch::Matchers::WithinAbs(3.0, 1e-8));
 
-    REQUIRE(r.getDirection().x() == 4.0);
-    REQUIRE(r.getDirection().y() == 5.0);
-    REQUIRE(r.getDirection().z() == 6.0);
+    REQUIRE_THAT(r.getDirection().x(), Catch::Matchers::WithinAbs(4.0, 1e-8));
+    REQUIRE_THAT(r.getDirection().y(), Catch::Matchers::WithinAbs(5.0, 1e-8));
+    REQUIRE_THAT(r.getDirection().z(), Catch::Matchers::WithinAbs(6.0, 1e-8));
 }
 
 TEST_CASE("Ray At Parameter Test"){
@@ -23,44 +23,44 @@ TEST_CASE("Ray At Parameter Test"){
     ray r(orig, dir);
 
     point3 at_point = r.at(2.0);
-    REQUIRE(at_point.x() == 9.0);
-    REQUIRE(at_point.y() == 12.0);
-    REQUIRE(at_point.z() == 15.0);
+    REQUIRE_THAT(at_point.x(), Catch::Matchers::WithinAbs(9.0, 1e-8));
+    REQUIRE_THAT(at_point.y(), Catch::Matchers::WithinAbs(12.0, 1e-8));
+    REQUIRE_THAT(at_point.z(), Catch::Matchers::WithinAbs(15.0, 1e-8));
 }
 
 TEST_CASE("Ray Default Constructor Test"){
     ray r;
 
-    REQUIRE(r.getOrigin().x() == 0.0);
-    REQUIRE(r.getOrigin().y() == 0.0);
-    REQUIRE(r.getOrigin().z() == 0.0);
+    REQUIRE_THAT(r.getOrigin().x(), Catch::Matchers::WithinAbs(0.0, 1e-8));
+    REQUIRE_THAT(r.getOrigin().y(), Catch::Matchers::WithinAbs(0.0, 1e-8));
+    REQUIRE_THAT(r.getOrigin().z(), Catch::Matchers::WithinAbs(0.0, 1e-8));
 
-    REQUIRE(r.getDirection().x() == 0.0);
-    REQUIRE(r.getDirection().y() == 0.0);
-    REQUIRE(r.getDirection().z() == 0.0);
+    REQUIRE_THAT(r.getDirection().x(), Catch::Matchers::WithinAbs(0.0, 1e-8));
+    REQUIRE_THAT(r.getDirection().y(), Catch::Matchers::WithinAbs(0.0, 1e-8));
+    REQUIRE_THAT(r.getDirection().z(), Catch::Matchers::WithinAbs(0.0, 1e-8));
 }
 
 TEST_CASE("Ray At Parameter Negatives And Zero Test"){
     point3 orig(1.0, 1.0, -1.0);
     vec3 dir(0.0, -1.0, 2.0);
     ray r(orig, dir);
-    REQUIRE(r.at(0.0).x() == 1.0);
-    REQUIRE(r.at(0.0).y() == 1.0);
-    REQUIRE(r.at(0.0).z() == -1.0);
-    REQUIRE(r.at(-1.0).x() == 1.0);
-    REQUIRE(r.at(-1.0).y() == 2.0);
-    REQUIRE(r.at(-1.0).z() == -3.0);
-    REQUIRE(r.at(3.0).x() == 1.0);
-    REQUIRE(r.at(3.0).y() == -2.0);
-    REQUIRE(r.at(3.0).z() == 5.0);
+    REQUIRE_THAT(r.at(0.0).x(), Catch::Matchers::WithinAbs(1.0, 1e-8));
+    REQUIRE_THAT(r.at(0.0).y(), Catch::Matchers::WithinAbs(1.0, 1e-8));
+    REQUIRE_THAT(r.at(0.0).z(), Catch::Matchers::WithinAbs(-1.0, 1e-8));
+    REQUIRE_THAT(r.at(-1.0).x(), Catch::Matchers::WithinAbs(1.0, 1e-8));
+    REQUIRE_THAT(r.at(-1.0).y(), Catch::Matchers::WithinAbs(2.0, 1e-8));
+    REQUIRE_THAT(r.at(-1.0).z(), Catch::Matchers::WithinAbs(-3.0, 1e-8));
+    REQUIRE_THAT(r.at(3.0).x(), Catch::Matchers::WithinAbs(1.0, 1e-8));
+    REQUIRE_THAT(r.at(3.0).y(), Catch::Matchers::WithinAbs(-2.0, 1e-8));
+    REQUIRE_THAT(r.at(3.0).z(), Catch::Matchers::WithinAbs(5.0, 1e-8));
 }
 
 TEST_CASE("At Parameter With Zero Direction Test"){
     point3 orig(2.0, 3.0, 4.0);
     vec3 dir(0.0, 0.0, 0.0);
     ray r(orig, dir);
-    REQUIRE(r.at(5.0).x() == 2.0);
-    REQUIRE(r.at(5.0).y() == 3.0);
-    REQUIRE(r.at(5.0).z() == 4.0);
+    REQUIRE_THAT(r.at(5.0).x(), Catch::Matchers::WithinAbs(2.0, 1e-8));
+    REQUIRE_THAT(r.at(5.0).y(), Catch::Matchers::WithinAbs(3.0, 1e-8));
+    REQUIRE_THAT(r.at(5.0).z(), Catch::Matchers::WithinAbs(4.0, 1e-8));
 }
 

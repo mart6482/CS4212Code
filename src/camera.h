f7f6144a00f53726d0b803c69601b3f024cae0ab
Andrew Martini <mart6482@d.umn.edu>
2026-01-30 21:34:23 -0600

    added more to the camera class

--- src/camera.h
+++ src/camera.h
@@ -2,10 +2,17 @@
 
 class Camera {
     public:
-        Camera() : pos(0,0,0), u(1,0,0), v(0,1,0), w(0,0,1), focalLength(1.0f), imageplaneWidth(0.25), imageplaneHeight(0.25){}
-        virtual void generateRay(int i, int j, ray &r) = 0;
+        Camera() : pos(0,0,0), u(1,0,0), v(0,1,0), w(0,0,1), focalLength(1.0f), imageplaneWidth(0.25), 
+        imageplaneHeight(0.25), nx(100), ny(100){}
+        virtual void generateRay(int i, int j, ray &r, int nx, int ny) = 0;
 
         virtual ~Camera() {};
+        
+
+        //fix/implement these later
+        //virtual rat generateRay(int i, int j) = 0;
+        //virtual void generate(float i, float j, ray &r) = 0;
+        //virtual void generate(float i, float j) = 0;
 
     protected:
         point3 pos;
@@ -13,5 +20,6 @@ class Camera {
         float focalLength;
         float imageplaneWidth;
         float imageplaneHeight;
+        int nx, ny;
         
 };

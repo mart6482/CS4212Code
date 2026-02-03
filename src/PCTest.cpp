482a77788a5757c7dd692456f106713d98135c50
Andrew Martini <mart6482@d.umn.edu>
2026-02-02 21:32:42 -0600

    changed image generation settings

--- src/PCTest.cpp
+++ src/PCTest.cpp
@@ -5,22 +5,27 @@ int main (){
     int width = 200;
     int height = 200;
     Framebuffer fb(width, height);
-    PerspectiveCamera cam(width, height);
+    vec3 position = vec3(3, -4, 12);
+    vec3 direction = vec3( -8, -5, 10.0);
+    float focalLength = 0.001f;
+    float imagePlaneWidth = 0.5f;
+    float imagePlaneHeight = 0.5f;
+
+    PerspectiveCamera customCam(position, direction, focalLength, imagePlaneWidth, imagePlaneHeight, width, height);
+    ray testRay = customCam.generateRay(width/2, height/2);
     fb.clearToColor(color(0.0, 0.0, 0.0));
     for (int j = 0; j < height; ++j) {
         for (int i = 0; i < width; ++i) {
-            ray r = cam.generateRay(i, j);
-            // For testing, we color pixels based on the ray direction
+            ray r = customCam.generateRay(i, j);
             color pixelColor = color(
-                0.5 * (r.getDirection().x() + 1.0),
-                0.5 * (r.getDirection().y() + 1.0),
-                0.5 * (r.getDirection().z() + 1.0)
+                0.5f * (r.getDirection().x() + 1.0f),
+                0.5f * (r.getDirection().y() + 1.0f),
+                0.5f * (r.getDirection().z() + 1.0f)
             );
             fb.setPixelColor(i, j, pixelColor);
         }
     }
-    fb.exportAsPNG("PCTestOutput.png");
-
+    fb.exportAsPNG("RayTracerTest.png");
 
     return 0;
 }

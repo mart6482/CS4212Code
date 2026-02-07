571cea764ca47da0816a555c11a8dafe56c5a10a
Andrew Martini <mart6482@d.umn.edu>
2026-02-06 20:30:22 -0600

    fixed a bug where intersect would incorrectly output true in cases where the ray is pointing away from the sphere

--- src/sphere.h
+++ src/sphere.h
@@ -10,11 +10,13 @@ class sphere : public shape{
             float b = 2.0f * dot(oc, r.getDirection());
             float c = dot(oc, oc) - radius * radius;
             float discriminant = b * b - 4 * a * c;
-            if(discriminant < 0){
+            if(discriminant < 0) {
                 return false;
-            } else {
-                return true;
             }
+            float sqrt_disc = std::sqrt(discriminant);
+            float t1 = (-b - sqrt_disc) / (2.0f * a);
+            float t2 = (-b + sqrt_disc) / (2.0f * a);
+            return (t1 >= 0) || (t2 >= 0);
         }
 
     private:

8d4522ea0ad5357f6b41dc290d8c4233a65f8550
Andrew Martini <mart6482@d.umn.edu>
2026-02-15 23:08:45 -0600

    created a hitstructure class

---
+++ src/HitStructure.h
@@ -0,0 +1,23 @@
+#ifndef HITTABLE_H
+#define HITTABLE_H
+
+#include "ray.h"
+
+class hit_record {
+  public:
+    point3 p;
+    vec3 normal;
+    double t;
+
+    hit_record() : p(point3(0,0,0)), normal(vec3(0,0,0)), t(0) {}
+};
+
+class hittable {
+  public:
+    virtual ~hittable() = default;
+    hittable() {}
+
+    virtual bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const = 0;
+};
+
+#endif

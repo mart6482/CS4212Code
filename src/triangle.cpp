17586e9faf7b541d2e15f8639808caf9f213a74d
Andrew Martini <mart6482@d.umn.edu>
2026-03-01 19:52:58 -0600

    refactored triangle class

--- src/triangle.cpp
+++ src/triangle.cpp
@@ -1,52 +1,56 @@
 #include "HitStructure.h"
 #include "Triangle.h"
 
-bool Triangle::intersect( const ray &r, float t_min, float t_max, hit_record& rec) const
+bool Triangle::intersect( const ray &r, float t_min, float& t_max, hit_record& rec) const
 {
-    double a = this->vertex_a[0] - this->vertex_b[0];
-    double b = this->vertex_a[1] - this->vertex_b[1];
-    double c = this->vertex_a[2] - this->vertex_b[2];
+    double a = vertex_a[0] - vertex_b[0];
+    double b = vertex_a[1] - vertex_b[1];
+    double c = vertex_a[2] - vertex_b[2];
 
-    double d = this->vertex_a[0] - this->vertex_c[0];
-    double e = this->vertex_a[1] - this->vertex_c[1];
-    double f = this->vertex_a[2] - this->vertex_c[2];
+    double d = vertex_a[0] - vertex_c[0];
+    double e = vertex_a[1] - vertex_c[1];
+    double f = vertex_a[2] - vertex_c[2];
 
     double g = r.getDirection()[0];
     double h = r.getDirection()[1];
     double i = r.getDirection()[2];
 
-    double j = this->vertex_a[0] - r.getOrigin()[0];
-    double k = this->vertex_a[1] - r.getOrigin()[1];
-    double l = this->vertex_a[2] - r.getOrigin()[2];
+    double j = vertex_a[0] - r.getOrigin()[0];
+    double k = vertex_a[1] - r.getOrigin()[1];
+    double l = vertex_a[2] - r.getOrigin()[2];
 
     double ei_hf = e * i - h * f;
-    double gf_di = g * f - d * i;
+    double gf_id = g * f - d * i;
     double dh_eg = d * h - e * g;
 
-    double M = a * ei_hf + b * gf_di + c* dh_eg;
+    double M = a * ei_hf + b * gf_id + c * dh_eg;
 
     double t = -1.0, gamma = -1.0, beta = -1.0;
 
-    beta = (j * ei_hf + k * gf_di + l * dh_eg) / M;
-    gamma = (i*(a * k - j * b) + h * (j * c - a * l) + g * (b * l - k * c)) / M;
-    t = -(f * (a * k - j * b) + e * (j * c - a * l) + d * (b * l - k * c)) / M;
-    double alpha = 1.0 - beta - gamma;
+    double ak_jb = a * k - j * b;
+    double jc_al = j * c - a * l;
+    double bl_kc = b * l - k * c;
 
-    if(t < 0.0) {
-        return false;
-    }
-    if (gamma < 0.0 || gamma > 1.0) {
-        return false;
-    }
-    if (beta < 0.0 || beta > 1.0 - gamma) {
-        return false;
-    }
+    t = -(f * ak_jb + e * jc_al + d * bl_kc) / M;
     if (t < t_min || t > t_max) {
         return false;
     }
-    
+
+    gamma = (i * ak_jb + h * jc_al + g * bl_kc) / M;
+    if (gamma < 0.0 || gamma > 1.0) {
+        return false;
+    }
+
+    beta = (j * ei_hf + k * gf_id + l * dh_eg) / M;
+    if (beta < 0.0 || beta > 1.0 - gamma) {
+        return false;
+    }
+
+    t_max = t;
     rec.t = t;
-    rec.p = r.at(rec.t);
+    rec.p = r.at(t);
+    rec.shape = this;
     rec.normal = normalize(cross(vertex_b - vertex_a, vertex_c - vertex_a));
+
     return true;
 }
--- src/triangle.h
+++ src/triangle.h
@@ -4,13 +4,13 @@
 #include "shape.h"
 #include "HitStructure.h"
 
-class Triangle : public shape {
+class Triangle : public Shape {
     public:
-        Triangle() : vertex_a(vec3(0.0,0.0,0.0)), vertex_b(vec3(1.0, 0.0, 0.0)), vertex_c(vec3(0.0, 1.0, 0.0)) { }
-        Triangle(const vec3& vertex_a, const vec3& vertex_b, const vec3 &vertex_c)
-            : vertex_a(vertex_a), vertex_b(vertex_b), vertex_c(vertex_c) { }
+        Triangle() : vertex_a(vec3(0, 0, 0)), vertex_b(vec3(1, 0, 0)), vertex_c(vec3(0, 1, 0)), color(vec3(1.0, 1.0, 1.0)), shader(nullptr) {}
+        Triangle(const vec3& a, const vec3& b, const vec3& c) : vertex_a(a), vertex_b(b), vertex_c(c), color(vec3(1.0, 1.0, 1.0)), shader(nullptr) {}
+        Triangle(const vec3& a, const vec3& b, const vec3& c, const vec3& col) : vertex_a(a), vertex_b(b), vertex_c(c), color(col), shader(nullptr) {}
 
-        bool intersect(const ray& r, float t_min, float t_max, hit_record& rec) const override;
+        bool intersect(const ray& r, float t_min, float& t_max, hit_record& rec) const override;
         std::shared_ptr<shader> getShader() const override {
                 return shader;
         }

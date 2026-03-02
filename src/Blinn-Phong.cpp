821c2c4d8bc8731cb987ead934d3580ef8b0b732
Andrew Martini <mart6482@d.umn.edu>
2026-03-01 19:25:25 -0600

    added support for multiple lights

--- src/Blinn-Phong.cpp
+++ src/Blinn-Phong.cpp
@@ -1,32 +1,34 @@
 #include "Blinn-Phong.h"
 #include "HitStructure.h"
 #include "color.h"
+#include <vector>
 
-color BlinnPhong::shade(const hit_record& hit) const {
-    vec3 light_dir = normalize(vec3(0.0f, 1.0f, 1.0f));
-    vec3 view_dir = normalize(-hit.p);
+color BlinnPhong::shade(const hit_record& hit, const std::vector<std::shared_ptr<PointLight>>& lights){
+    vec3 materialColor = hit.shape->getcolor();
+    vec3 color = vec3(0.0f, 0.0f, 0.0f);
 
-    // Compute Lambertian component
-    float nDotL = std::max(0.0, dot(hit.normal, light_dir));
-    color LambertianComponent = color(nDotL, nDotL, nDotL);
-
-    // Compute Blinn-Phong component
-    vec3 halfVector = normalize(view_dir + light_dir);
-    float nDotH = std::max(0.0, dot(hit.normal, halfVector));
-    float BlinnPhongExponent = 32.0f;
-    color BlinnPhongComponent = color(pow(nDotH, BlinnPhongExponent), pow(nDotH, BlinnPhongExponent), pow(nDotH, BlinnPhongExponent));
-    color result = LambertianComponent + BlinnPhongComponent;
-
-    //clamp the result to [0, 1]
-    if (result.x() > 1.0f) result[0] = 1.0f;
-    if (result.y() > 1.0f) result[1] = 1.0f;
-    if (result.z() > 1.0f) result[2] = 1.0f;
-    if (result.x() < 0.0f) result[0] = 0.0f;
-    if (result.y() < 0.0f) result[1] = 0.0f;
-    if (result.z() < 0.0f) result[2] = 0.0f;
-    return result;
-
+    float kd = 1.0f;
+    float ks = 0.6f;
+    float p = 32.0f;
     
+    vec3 view_dir = normalize(eyePosition - hit.p);
+
+    for(const auto& light: lights){
+        vec3 light_dir = normalize(light->getPosition() - hit.p);
+        vec3 half_vector = normalize(light_dir + view_dir);
+
+        float diff = std::max(0.0, dot(hit.normal, light_dir));
+        vec3 diffuse = kd * diff * light->getColor() * light->getIntensity();
+        
+
+        float spec = std::pow(std::max(0.0, dot(hit.normal, half_vector)), p);
+        vec3 specular = ks * spec * light->getColor() * light->getIntensity();
+
+        color += diffuse * materialColor + specular;
+    }
+
+    color = vec3(std::min(color.x(), 1.0), std::min(color.y(), 1.0), std::min(color.z(), 1.0));
+    return color;
 }
 
 BlinnPhong::~BlinnPhong() {}
--- src/Blinn-Phong.h
+++ src/Blinn-Phong.h
@@ -5,5 +5,9 @@ class BlinnPhong : public shader {
     public:
         BlinnPhong() {}
         virtual ~BlinnPhong();
-        virtual color shade(const hit_record& hit) const override;
+        virtual color shade(const hit_record& hit, const std::vector<std::shared_ptr<PointLight>>& lights) override;
+        void setEyePosition(const vec3& eyePos) { eyePosition = eyePos; }
+
+    private:
+        vec3 eyePosition = vec3(0.0f, 0.0f, 0.0f);
 };

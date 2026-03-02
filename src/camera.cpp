4ef9d328c354f2954872656f16ea4f263ee427d8
Andrew Martini <mart6482@d.umn.edu>
2026-03-01 23:27:07 -0600

    added generateRay functions that accept floats

--- src/camera.cpp
+++ src/camera.cpp
@@ -16,3 +16,24 @@ Camera::Camera( int pixel_nx, int pixel_ny )
     float aspectRatio = static_cast<float>(pixel_nx) / static_cast<float>(pixel_ny);
     imagePlaneHeight = imagePlaneWidth / aspectRatio;
 }
+
+Camera::Camera(vec3 position, vec3 viewDir, vec3 upDir, float focal_length, float image_plane_width, float image_plane_height, int pixel_nx, int pixel_ny)
+: pos(position),
+   focalLength(focal_length),
+   imagePlaneWidth(image_plane_width),
+   imagePlaneHeight(image_plane_height),
+   nx(pixel_nx),
+   ny(pixel_ny)
+{
+    W = -unit_vector(viewDir);
+    
+    vec3 t = upDir;
+
+    // If the up direction is parallel to the view direction, we need to choose a different up vector to avoid a degenerate camera basis.
+    if (std::abs(dot(unit_vector(t), W)) > 0.999f) {
+        t = vec3(0, 0, 1); // Choose a different up vector
+    }
+
+    U = unit_vector(cross(t, W));
+    V = cross(W, U);
+}
--- src/camera.h
+++ src/camera.h
@@ -5,18 +5,17 @@ class Camera {
     public:
         Camera();
         Camera( int pixel_nx, int pixel_ny );
+        Camera(vec3 position, vec3 viewDir, vec3 upDir, float focal_length, float image_plane_width, float image_plane_height, int pixel_nx, int pixel_ny);
 
         //virtual void generateRay(int i, int j, ray &r, int nx, int ny) = 0;
         virtual ray generateRay( int i, int j ) = 0;
+        virtual ray generateRay(float i, float j) = 0;
 
         virtual ~Camera() {};
         vec3 getPosition() const { return pos; }
         
 
-        //fix/implement these later
-        //virtual ray generateRay(int i, int j) = 0;
-        //virtual void generate(float i, float j, ray &r) = 0;
-        //virtual void generate(float i, float j) = 0;
+        
 
     protected:
         vec3 pos;

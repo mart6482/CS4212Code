b8f84b0f26150e88a2fb90fe3820a50acaea8d8d
Andrew Martini <mart6482@d.umn.edu>
2026-03-29 15:31:56 -0500

    updated vertex shader

--- OpenGL/vertexShader_passthrough.glsl
+++ OpenGL/vertexShader_passthrough.glsl
@@ -1,11 +1,16 @@
 #version 330 core
 
-layout(location =0) in vec3 in_Position;
-layout(location =1) in vec3 in_Color;
+layout(location=0) in vec3 in_Position;
+layout(location=1) in vec3 in_Color;
 
-out vec3 vColor; 
+uniform mat4 projMatrix;
+uniform mat4 viewMatrix;
+uniform mat4 modelMatrix;
 
-void main(void) {
-    vColor = in_Color;
-    gl_Position = vec4(in_Position , 1.0);
+out vec3 color;
+
+void main(void)
+{
+  color = in_Color;
+  gl_Position = projMatrix * viewMatrix * modelMatrix * vec4(in_Position, 1.0);
 }
